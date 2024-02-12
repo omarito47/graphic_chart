dc9bf29b0d6716f17f1ef54a5a74aec8ca744b84
omart <omart@TAAMALLAH-OMAR>
2024-02-12 14:53:29 +0100

    chart bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,223 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Bar Chart Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final List<TourneeData> tournees = [
+    TourneeData(
+      mois: 'Mars',
+      annee: 2023,
+      realises: 25,
+      date: [
+        DayData(jour: 12, realises: 1),
+        DayData(jour: 17, realises: 3),
+        DayData(jour: 25, realises: 2),
+      ],
+    ),
+    TourneeData(
+      mois: 'Avril',
+      annee: 2023,
+      realises: 18,
+      date: [
+        DayData(jour: 5, realises: 2),
+        DayData(jour: 14, realises: 1),
+        DayData(jour: 23, realises: 3),
+      ],
+    ),
+    TourneeData(
+      mois: 'Mai',
+      annee: 2023,
+      realises: 30,
+      date: [
+        DayData(jour: 2, realises: 4),
+        DayData(jour: 10, realises: 2),
+        DayData(jour: 19, realises: 3),
+      ],
+    ),
+    TourneeData(
+      mois: 'Juin',
+      annee: 2023,
+      realises: 22,
+      date: [
+        DayData(jour: 7, realises: 1),
+        DayData(jour: 16, realises: 2),
+        DayData(jour: 28, realises: 3),
+      ],
+    ),
+    TourneeData(
+      mois: 'Juillet',
+      annee: 2023,
+      realises: 27,
+      date: [
+        DayData(jour: 4, realises: 2),
+        DayData(jour: 13, realises: 3),
+        DayData(jour: 21, realises: 1),
+      ],
+    ),
+    TourneeData(
+      mois: 'Août',
+      annee: 2023,
+      realises: 20,
+      date: [
+        DayData(jour: 8, realises: 1),
+        DayData(jour: 16, realises: 2),
+        DayData(jour: 25, realises: 1),
+      ],
+    ),
+    TourneeData(
+      mois: 'Septembre',
+      annee: 2023,
+      realises: 23,
+      date: [
+        DayData(jour: 2, realises: 2),
+        DayData(jour: 10, realises: 3),
+        DayData(jour: 19, realises: 1),
+      ],
+    ),
+  ];
 
-  void _incrementCounter() {
+  String selectedInterval = 'Mois';
+
+  void changeInterval(String interval) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      selectedInterval = interval;
     });
+  }
+
+  List<charts.Series<IntervalData, String>> _createChartData() {
+    List<IntervalData> data = [];
+
+    if (selectedInterval == 'Mois') {
+      for (var tournee in tournees) {
+        data.add(IntervalData(
+          interval: tournee.mois,
+          count: tournee.realises,
+        ));
+      }
+    } else if (selectedInterval == 'Année') {
+      for (var tournee in tournees) {
+        data.add(IntervalData(
+          interval: tournee.annee.toString(),
+          count: tournee.realises,
+        ));
+      }
+    } else {
+      for (var tournee in tournees) {
+        for (var day in tournee.date) {
+          if (selectedInterval == 'Jour' && day.jour != null) {
+            data.add(IntervalData(
+              interval: day.jour.toString(),
+              count: day.realises,
+            ));
+          }
+        }
+      }
+    }
+
+    return [
+      charts.Series<IntervalData, String>(
+        id: 'tournees',
+        domainFn: (IntervalData data, _) => data.interval,
+        measureFn: (IntervalData data, _) => data.count,
+        data: data,
+      )
+    ];
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Bar Chart Demo'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => changeInterval('Jour'),
+                  child: Text('Jour'),
+                ),
+                ElevatedButton(
+                  onPressed: () => changeInterval('Mois'),
+                  child: Text('Mois'),
+                ),
+                ElevatedButton(
+                  onPressed: () => changeInterval('Année'),
+                  child: Text('Année'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16.0),
+              child: charts.BarChart(
+                _createChartData(),
+                animate: true,
+                domainAxis: charts.OrdinalAxisSpec(
+                  renderSpec: charts.SmallTickRendererSpec(
+                    labelRotation: 60,
+                  ),
+                ),
+                primaryMeasureAxis: charts.NumericAxisSpec(
+                  renderSpec: charts.SmallTickRendererSpec(),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class TourneeData {
+  final String mois;
+  final int annee;
+  final int realises;
+  final List<DayData> date;
+
+  TourneeData({
+    required this.mois,
+    required this.annee,
+    required this.realises,
+    required this.date,
+  });
+}
+
+class DayData {
+  final int jour;
+  final int realises;
+
+  DayData({required this.jour, required this.realises});
+}
+
+class IntervalData {
+  final String interval;
+  final int count;
+
+  IntervalData({required this.interval, required this.count});
 }
---
+++ lib/models/tournee.dart
@@ -0,0 +1,66 @@
+class Tournee {
+  final String mois;
+  final int annee;
+  final int realises;
+  final List<TourneeDate> dates;
+
+  Tournee({
+    required this.mois,
+    required this.annee,
+    required this.realises,
+    required this.dates,
+  });
+
+  factory Tournee.fromJson(Map<String, dynamic> json) {
+    final mois = json['mois'] as String;
+    final annee = json['annee'] as int;
+    final realises = json['realises'] as int;
+    final List<dynamic> datesJson = json['dates'];
+    final dates = datesJson.map((dateJson) => TourneeDate.fromJson(dateJson)).toList();
+
+    return Tournee(
+      mois: mois,
+      annee: annee,
+      realises: realises,
+      dates: dates,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final List<Map<String, dynamic>> datesJson = dates.map((date) => date.toJson()).toList();
+
+    return {
+      'mois': mois,
+      'annee': annee,
+      'realises': realises,
+      'dates': datesJson,
+    };
+  }
+}
+
+class TourneeDate {
+  final int jour;
+  final int realises;
+
+  TourneeDate({
+    required this.jour,
+    required this.realises,
+  });
+
+  factory TourneeDate.fromJson(Map<String, dynamic> json) {
+    final jour = json['jour'] as int;
+    final realises = json['realises'] as int;
+
+    return TourneeDate(
+      jour: jour,
+      realises: realises,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'jour': jour,
+      'realises': realises,
+    };
+  }
+}
